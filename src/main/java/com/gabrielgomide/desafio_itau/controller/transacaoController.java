611bf28b0b198dec4390ac76eac1b6370f2281f0
Gomide-Gabriel <nephilin196@gmail.com>
2026-05-03 20:33:47 -0300

    testando controllers

--- src/main/java/com/gabrielgomide/desafio_itau/controller/transacaoController.java
+++ src/main/java/com/gabrielgomide/desafio_itau/controller/transacaoController.java
@@ -1,4 +1,13 @@
 package com.gabrielgomide.desafio_itau.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class transacaoController {
+
+    @GetMapping("/teste")
+    public String transacaoTest(){
+        return "transacao";
+    }
 }
